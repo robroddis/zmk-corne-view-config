427f64db0d01cf2a8dce33d4f3fa4ecba39c22f7
Rob Roddis <robert.roddis@gmail.com>
2023-09-02 22:04:53 +0100

    Build the actual config
    Fix build_side.sh to provide the path to config for west build.
    Fix keymap to:
    
     * put windows layers first
     * QWERTY on windows.
     * combo J,K for Esc
     * put BSPC on r-thumb

--- build_side.sh
+++ build_side.sh
@@ -1,9 +1,10 @@
 . ./env_sided.sh
-export HOME=/work
+export HOME=/work/home
+mkdir -p ${HOME}
 west init -l "config"
 west update
 west zephyr-export
-west build -s zmk/app -b "nice_nano_v2" -- -DZMK_CONFIG="${GITHUB_WORKSPACE}/config" -DSHIELD="corne_${shield_side} nice_view_adapter nice_view"
+west build -s zmk/app -b "nice_nano_v2" -- -DZMK_CONFIG="${PWD}/config" -DSHIELD="corne_${shield_side} nice_view_adapter nice_view"
 mkdir -p build/artifacts
 cp build/zephyr/zmk.uf2 "build/artifacts/corne_${shield_side} nice_view_adapter nice_view-nice_nano_v2-zmk.uf2"
 
